db2e514e7502e33e659ed4cfae1dd1e1079afb03
KOSA <zoooz0616@gmail.com>
2023-08-21 17:14:47 +0900

    [eunji][#add] 초기 프로젝트 생성

---
+++ KosaESS/src/main/java/com/finalprj/kess/KosaEssApplication.java
@@ -0,0 +1,13 @@
+package com.finalprj.kess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosaEssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosaEssApplication.class, args);
+	}
+
+}
---
+++ KosaESS/src/test/java/com/finalprj/kess/KosaEssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalprj.kess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosaEssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
