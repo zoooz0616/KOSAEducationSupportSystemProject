b65478d16b574b331025ef64a040e32297b85bd0
KOSA <zoooz0616@gmail.com>
2023-09-20 11:30:26 +0900

    [eunji][#update]기업 구현완료

--- KosaESS/src/main/java/com/finalprj/kess/service/IAdminService.java
+++ KosaESS/src/main/java/com/finalprj/kess/service/IAdminService.java
@@ -131,4 +131,10 @@ public interface IAdminService {
 	void insertCompanyVO(FileVO fileVO, CompanyVO companyVO);
 	CompanyVO getCompanyVO(String cmpyId);
 	void deleteCompany(List<String> selectedCompanyIds);
+	int getTpcdIdCnt(String tpcdId);
+	String getMaxGroupCodeId();
+	void insertGroupCode(CommonCodeVO commonCodeVO);
+	void deleteGroupCode(List<String> selectedGroupCodeIds);
+	void updateGroupCode(String cmcdId, String cmcdNm);
+	List<CommonCodeVO> getDetailCodeList(String cmcdId);
 }
